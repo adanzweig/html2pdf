8846ab019ea557810c0f17d0f3f0ddd45b10fd78
Adan Zweig <adanzweig+1@gmail.com>
2025-04-24 14:04:05 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, send_file
+from xhtml2pdf import pisa
+import io
+
+app = Flask(__name__)
+
+@app.route('/generate-pdf', methods=['POST'])
+def generate_pdf():
+    html = request.data.decode('utf-8')
+    result = io.BytesIO()
+
+    pisa_status = pisa.CreatePDF(io.StringIO(html), dest=result)
+    result.seek(0)
+
+    if pisa_status.err:
+        return {'error': 'Error generating PDF'}, 500
+
+    return send_file(result, mimetype='application/pdf', as_attachment=True, download_name='documento.pdf')
+
+if __name__ == '__main__':
+    app.run(debug=True)
